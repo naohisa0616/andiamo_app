700e8cde68d57449af9e587c756e8119d8c0fee7
宮崎直久 <naohisa0616@icloud.com>
2024-10-14 16:57:29 +0900

    【追加】Firebaseの認証処理機能を持つAuthenticationクラスを追加

---
+++ lib/Utils/Auth/authentication.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../Model/Account.dart';
+
+class Authentication {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static User? currentFirebaseUser;
+  static Account? myAccount;
+
+  static Future<dynamic> signUp({required email, required pass}) async {
+      try {
+        UserCredential newAccount = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: pass);
+        print('auth登録完了');
+        return newAccount;
+      } on FirebaseAuthException catch(e) {
+        print('auth登録エラー: $e');
+        return false;
+      }
+  }
+
+  static Future<dynamic> emailSignIn({required String email, required String pass}) async {
+    try {
+      final UserCredential _result = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email,
+          password: pass
+      );
+      currentFirebaseUser = _result.user;
+      print('authサインイン完了');
+      return _result;
+    } on FirebaseAuthException catch(e) {
+      print('authサインインエラー: $e');
+      return false;
+    }
+  }
+
+  static Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  static Future<void> deleteAuth() async {
+    await currentFirebaseUser!.delete();
+  }
+}
