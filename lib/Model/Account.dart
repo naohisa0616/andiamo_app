b5245a65cace093ef5a5cfd1bf13ad3157f449cc
宮崎直久 <naohisa0616@icloud.com>
2024-10-14 16:55:30 +0900

    【追加】Accountファイル追加

---
+++ lib/Model/Account.dart
@@ -0,0 +1,9 @@
+class Account {
+  String userID;
+  String userName;
+  String iconImage;
+  String selfIntroductionText;
+
+  Account({this.userID = '', this.userName = '', this.iconImage = '',
+    this.selfIntroductionText = ''});
+}
